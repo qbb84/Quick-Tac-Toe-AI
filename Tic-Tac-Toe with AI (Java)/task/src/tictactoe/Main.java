6715d94f77abe3c9a37f32b18377aa647630e8ed
luke <surgerewind@gmail.com>
2023-09-27 17:25:23 +0100

    Initial commit

--- Tic-Tac-Toe with AI (Java)/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe with AI (Java)/task/src/tictactoe/Main.java
@@ -5,12 +5,10 @@ import tictactoe.Game.TicTacToe;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        // write your code here
 
         TicTacToe game = new TicTacToe(Player.PLAYER_1, Player.PLAYER_2);
         game.setupInitialState();
         game.run();
-
 
         }
 
